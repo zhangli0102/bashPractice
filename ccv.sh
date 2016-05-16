8e78b9e8c8a9529c496782f1e1a4d2aabdb93601
zhangli0102 <zhangli@gwu.edu>
2016-05-16 12:23:13 -0700

    update ccv file

--- ccv.sh
+++ ccv.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 value1=$(<../presta/CONTRACT_VERSION)
 value2=$(<../theseus/CONTRACT_VERSION)
+
+EXIT=1 #expecting failure
 if [ $value1 -eq $value2 ] 
+#then
+#  echo "Contract Version Matched!"
+#else
+#  echo "Contract Version Unmatched!"
 then
-  echo "Contract Version Matched!"
-else
-  echo "Contract Version Unmatched!"
+  EXIT=0
 fi
+
+exit $EXIT
