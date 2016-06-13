d67b1f938810e762c56f988d9424fb2b4b94ed6c
zhangli0102 <zhangli@gwu.edu>
2016-06-13 13:37:43 -0700

    Try to find fail reason

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 #/bin/bash
 value1=$(<CONTRACT_VERSION)
 value2=$(<../sr/CONTRACT_VERSION)
-echo $value1
-echo $value2
 EXIT=1 #expecting failure
 if [ $value1 -eq $value2 ]; then
   echo Sucessed!
-  EXIT=1
+  EXIT=0
 else
   echo Failed.
 fi
