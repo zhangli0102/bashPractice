beb78b23c9c3b81c8ca9a64a5a56e56acfb787d2
Li Zhang <zhangli@gwu.edu>
2017-04-25 18:19:42 +0000

    if

---
+++ sh27.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo What cars do you have?
+
+read car1 car2 car3
+
+echo Your first car is $car1
+echo Your second car is $car2
---
+++ sh28.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $1 -gt 1000 ]
+then
+  echo What a large number!
+fi
+
+date
